c63ed8fbea6ebd90efa076020aaa3025a978e79c
Bobris <Boris.Letocha@gmail.com>
2013-07-16 00:27:53 +0200

    Ssl impl continues.

--- NowinWebServer/SslTransportHandler.cs
+++ NowinWebServer/SslTransportHandler.cs
@@ -18,6 +18,7 @@ namespace NowinWebServer
         Task _authenticateTask;
         int _recvOffset;
         int _recvLength;
+        readonly InputStream _inputStream;
 
         public SslTransportHandler(ITransportLayerHandler next, X509Certificate serverCertificate, byte[] buffer, int startBufferOffset, int bufferSize)
         {
@@ -27,21 +28,25 @@ namespace NowinWebServer
             EncryptedReceiveBufferOffset = startBufferOffset;
             EncryptedSendBufferOffset = startBufferOffset + bufferSize;
             _bufferSize = bufferSize;
-            _ssl = new SslStream(new InputStream(this), true);
+            _inputStream = new InputStream(this);
+            _ssl = new SslStream(_inputStream, true);
         }
 
         class InputStream : Stream
         {
             readonly SslTransportHandler _owner;
+            readonly byte[] _buf;
+            TaskCompletionSource<int> _tcs;
+            int _receivePos;
+            int _receiveLen;
+            byte[] _asyncBuffer;
+            int _asyncOffset;
+            int _asyncCount;
 
             public InputStream(SslTransportHandler owner)
             {
                 _owner = owner;
-            }
-
-            public override void Flush()
-            {
-                throw new NotImplementedException();
+                _buf = owner._buffer;
             }
 
             public override long Seek(long offset, SeekOrigin origin)
@@ -54,7 +59,70 @@ namespace NowinWebServer
                 throw new InvalidOperationException();
             }
 
+            public void FinishAccept(int offset, int length)
+            {
+                _receivePos = offset;
+                _receiveLen = length;
+            }
+
+            public void FinishReceive(int offset, int length)
+            {
+                _receivePos = offset;
+                _receiveLen = length;
+                var l = Math.Min(_asyncCount, _receiveLen);
+                Array.Copy(_buf, _receivePos, _asyncBuffer, _asyncOffset, l);
+                _receivePos += l;
+                _receiveLen -= l;
+                _tcs.TrySetResult(l);
+            }
+
+            public void FinishReceiveWithAbort()
+            {
+                _tcs.TrySetCanceled();
+            }
+
             public override int Read(byte[] buffer, int offset, int count)
+            {
+                if (_receiveLen > 0)
+                {
+                    var l = Math.Min(count, _receiveLen);
+                    Array.Copy(_buf, _receivePos, buffer, offset, l);
+                    _receivePos += l;
+                    _receiveLen -= l;
+                    return l;
+                }
+                return ReadOverflowAsync(buffer, offset, count).Result;
+            }
+
+            public override Task<int> ReadAsync(byte[] buffer, int offset, int count, System.Threading.CancellationToken cancellationToken)
+            {
+                if (_receiveLen > 0)
+                {
+                    var l = Math.Min(count, _receiveLen);
+                    Array.Copy(_buf, _receivePos, buffer, offset, l);
+                    _receivePos += l;
+                    _receiveLen -= l;
+                    return Task.FromResult(l);
+                }
+                return ReadOverflowAsync(buffer, offset, count);
+            }
+
+            Task<int> ReadOverflowAsync(byte[] buffer, int offset, int count)
+            {
+                _asyncBuffer = buffer;
+                _asyncOffset = offset;
+                _asyncCount = count;
+                _tcs = new TaskCompletionSource<int>();
+                _owner.Callback.StartReceive(_owner.EncryptedReceiveBufferOffset, _owner._bufferSize);
+                return _tcs.Task;
+            }
+
+            public void FinishSend(Exception exception)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void Flush()
             {
                 throw new NotImplementedException();
             }
@@ -101,36 +169,44 @@ namespace NowinWebServer
 
         public void FinishAccept(int offset, int length)
         {
-            _authenticateTask = _ssl.AuthenticateAsServerAsync(_serverCertificate).ContinueWith((t, selfObject) =>
+            _inputStream.FinishAccept(offset, length);
+            try
+            {
+                _authenticateTask = _ssl.AuthenticateAsServerAsync(_serverCertificate).ContinueWith((t, selfObject) =>
                 {
                     var self = (SslTransportHandler)selfObject;
                     if (t.IsFaulted || t.IsCanceled)
                         self._next.FinishAccept(self._recvOffset, 0);
                     else
                         self._ssl.ReadAsync(self._buffer, self._recvOffset, self._recvLength).ContinueWith((t2, selfObject2) =>
-                            {
-                                var self2 = (SslTransportHandler)selfObject2;
-                                if (t2.IsFaulted || t2.IsCanceled)
-                                    self2._next.FinishAccept(self2._recvOffset, 0);
-                                else
-                                    self2._next.FinishAccept(self2._recvOffset, t2.Result);
-                            }, self);
+                        {
+                            var self2 = (SslTransportHandler)selfObject2;
+                            if (t2.IsFaulted || t2.IsCanceled)
+                                self2._next.FinishAccept(self2._recvOffset, 0);
+                            else
+                                self2._next.FinishAccept(self2._recvOffset, t2.Result);
+                        }, self);
                 }, this);
+            }
+            catch (Exception)
+            {
+                Callback.StartDisconnect();
+            }
         }
 
         public void FinishReceive(int offset, int length)
         {
-            throw new NotImplementedException();
+            _inputStream.FinishReceive(offset, length);
         }
 
         public void FinishReceiveWithAbort()
         {
-            throw new NotImplementedException();
+            _inputStream.FinishReceiveWithAbort();
         }
 
         public void FinishSend(Exception exception)
         {
-            throw new NotImplementedException();
+            _inputStream.FinishSend(exception);
         }
 
         public void StartAccept(int offset, int length)
@@ -176,7 +252,16 @@ namespace NowinWebServer
 
         public void StartDisconnect()
         {
-            Callback.StartDisconnect();
+            var t = _authenticateTask;
+            _authenticateTask = null;
+            if (t != null)
+            {
+                t.ContinueWith((t2, callback) => ((ITransportLayerCallback) callback).StartDisconnect(), Callback);
+            }
+            else
+            {
+                Callback.StartDisconnect();
+            }
         }
     }
 }
