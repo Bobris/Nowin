737f0c2eb8e19e4791e008a5e43c3bb772714f21
Bobris <Boris.Letocha@gmail.com>
2013-08-11 01:10:45 +0200

    Rename finished, Bumped version to 0.9

--- Nowin/Properties/AssemblyInfo.cs
+++ Nowin/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NowinWebServer")]
+[assembly: AssemblyTitle("Nowin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Boris Letocha")]
-[assembly: AssemblyProduct("NowinWebServer")]
+[assembly: AssemblyProduct("Nowin")]
 [assembly: AssemblyCopyright("Copyright © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
